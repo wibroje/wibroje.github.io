3aa295d4d930dead18da319f7c0d89e53e791909
William Jefferies <willjefferies10@gmail.com>
2018-06-23 20:48:45 -0600

    revamp

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Up and running!")
+$(document).ready(function(){
+const log = console.log
+
+  	M.AutoInit();
+
+	$('.parallax').parallax();
+	$('.carousel').carousel();
+
+
+
+	$.ajax({
+		url: 'https://icanhazdadjoke.com/slack',
+		method: 'GET',
+		success: function(response){
+			const data = response.attachments[0].text;
+			log(data)
+			$('.dadjoke').html("<h3>A random dad joke for you:</h3><br><p>"+data+"</p>")
+		}
+	})
+
+})
+
+
+
+
